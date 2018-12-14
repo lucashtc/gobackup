436cbfa717d00e3ef38ed930819a759c87d2f533
Lucas Henrique Teixeira Coelho Dias <lucashenrique-teixeira@hotmail.com>
2018-12-14 15:51:29 -0200

    Alterado forma de obter nome das bases.

--- mysql/getdatabase.go
+++ mysql/getdatabase.go
@@ -4,37 +4,45 @@ package mysql
 
 // package responsável por obter informações do banco, como nome das bases e tabelas nas bases
 import (
+	"database/sql"
 	"fmt"
-	"strings"
 
-	"github.com/lucashtc/gobackup/execmysql"
+	// Interface
+	_ "github.com/go-sql-driver/mysql"
 )
+
+// Conn connetion database
+func Conn(conf DataBase) (*sql.DB, error) {
+	pas := fmt.Sprintf(":%s", conf.Password)
+	db, err := sql.Open("mysql", fmt.Sprintf("%s%s@/information_schema", conf.User, pas))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
 
 // GetDatabase excute command for getting name all databases
 func GetDatabase(cf DataBase) ([]string, error) {
-	var newStringStmt []string
-	var command []string
-	command = append(command, "-B")
-	command = append(command, "-s")
-	command = append(command, "-u", cf.User)
-	if cf.Password != "" {
-		command = append(command, fmt.Sprintf("-p%s", cf.Password))
-	}
-	command = append(command, "-e", "SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('mysql','information_schema','performance_schema') ")
-
-	out, err := execmysql.Exec(command)
+	var dataBases []string
+	db, err := Conn(cf)
 	if err != nil {
-		return []string{}, fmt.Errorf("Falha ao pegar name das bases >> %s", err)
+		return nil, err
 	}
-
-	stringStmt := strings.Split(string(out), "\r\n")
-
-	for _, v := range stringStmt {
-		if v != "" {
-			newStringStmt = append(newStringStmt, v)
+	defer db.Close()
+	result, err := db.Query("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('mysql','information_schema','performance_schema') ")
+	if err != nil {
+		return nil, err
+	}
+	for result.Next() {
+		var schema string
+		err := result.Scan(&schema)
+		if err != nil {
+			return nil, err
 		}
+		dataBases = append(dataBases, schema)
 	}
-	return newStringStmt, nil
+
+	return dataBases, nil
 }
 
 // GetData function get name databse
--- mysql/getdatabase_test.go
+++ mysql/getdatabase_test.go
@@ -16,7 +16,7 @@ func Test_GetDataBase(t *testing.T) {
 		t.Errorf("Falha os obter name database >> %s", err)
 	}
 	//Funciona com go test -v mysql
-	t.Logf("Result %s \n", bases)
+	t.Logf("Result %v \n", bases)
 }
 
 func Test_GetData(t *testing.T) {
@@ -30,4 +30,20 @@ func Test_GetData(t *testing.T) {
 	}
 
 	helper.PrettyPrint(r)
+}
+
+func Test_Conn(t *testing.T) {
+	conf := mysql.DataBase{}
+	conf.User = "root"
+
+	db, err := mysql.Conn(conf)
+	if err != nil {
+		t.Fatalf("Falha ao obter nome dos Schemas. Error: %s", err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		t.Fatalf("Falha ao Pinga servidor de banco. Error: %s", err)
+	}
+
 }
