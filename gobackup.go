2ad6fc73b48799884bbce53845fc50c0bcb8839c
Lucas Henrique Teixeira Coelho Dias <lucashenrique-teixeira@hotmail.com>
2018-12-17 16:01:24 -0200

    ...

--- gobackup.go
+++ gobackup.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"runtime"
+
 	"github.com/lucashtc/gobackup/arg"
 )
 
 //. "github.com/logrusorgru/aurora"
 
 func main() {
+	maxProcs := runtime.NumCPU()
+	runtime.GOMAXPROCS(maxProcs)
 	arg.Arg()
 }
